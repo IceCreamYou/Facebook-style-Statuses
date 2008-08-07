6d98c0750a648ddb94c878efa56b796657d9cb01
Isaac Sukin <ice@icecreamyou.com>
2008-08-07 00:55:38 +0000

    Code styling and general bug fixes.

--- facebook_status.js
+++
@@ -1,7 +0,0 @@
-if (Drupal.jsEnabled) {
-  $(document).ready(function () {
-    $('div.facebook_status_block').find('div.facebook_status_form').hide().end().find('div.facebook_status_status').click(function() {
-      $('div.facebook_status_form').slideToggle();
-    });
-  });
-}
