25ef1dd91495b7d9c560bc266f2e5d2bcc6b79b4
Isaac Sukin <ice@icecreamyou.com>
2009-02-26 04:57:17 +0000

    #369052 Reset form on click (with inspiration from dave_trigeia) #381702 Token conflict #380648 Cleared statuses show in Activity #314950 Activity 6.1/5.4 integration #371074 Author pane integration problem? #341360 Better cleared status text

--- facebook_status.js
+++ facebook_status.js
@@ -1,7 +1,31 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function () {
+    facebook_status_count = 1;
+    facebook_status_original_value = $('div.facebook_status_block').find('input#edit-name').val();
+    //Show or hide status form.
     $('div.facebook_status_block').find('div.facebook_status_form').hide().end().find('div.facebook_status_status').click(function() {
       $('div.facebook_status_form').slideToggle();
     });
+    $('div.facebook_status_block').find('input#edit-name').focus(function() {
+      facebook_status_form_clear();
+    });
   });
+  function facebook_status_form_clear() {
+    facebook_status_value = $('div.facebook_status_block').find('input#edit-name').val();
+    //Clear field on focus unless new text has been entered.
+    if (facebook_status_count == 1) {
+      facebook_status_value = "";
+      $('div.facebook_status_block').find('input#edit-name').val(facebook_status_value);
+    }
+    $('div.facebook_status_block').find('input#edit-name').keypress(function() {
+      facebook_status_count = 0;
+    });
+    //Set to original value on unfocus if field is empty.
+    $('div.facebook_status_block').find('input#edit-name').blur(function() {
+      if ($('div.facebook_status_block').find('input#edit-name').val() == "") {
+        facebook_status_value = facebook_status_original_value;
+        $('div.facebook_status_block').find('input#edit-name').val(facebook_status_value);
+      }
+    });
+  }
 }
