8876b93049a1a409741a2e9b4a18e356c098a9db
Isaac Sukin <ice@icecreamyou.com>
2009-03-31 02:23:04 +0000

    JS for slider/default text works now Fixed theme_facebook_status_time() Fixed Exclude Users setting won't validate Other general cleanup and small fixes

--- facebook_status.js
+++ facebook_status.js
@@ -1,7 +1,31 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function () {
-    $('div.facebook_status_block').find('div.facebook_status_form').hide().end().find('div.facebook_status_status').click(function() {
-      $('div.facebook_status_form').slideToggle();
+    facebook_status_count = 1;
+    facebook_status_original_value = $('div#facebook_status_update').find('input#edit-status').val();
+    //Show or hide status form.
+    $('div#facebook_status_update').find('span.facebook_status_hide').hide().end().find('a.facebook_status_slide').click(function() {
+      $('span.facebook_status_hide').slideToggle();
+    });
+    $('div#facebook_status_update').find('input#edit-status').focus(function() {
+      facebook_status_form_clear();
     });
   });
+  function facebook_status_form_clear() {
+    facebook_status_value = $('div#facebook_status_update').find('input#edit-status').val();
+    //Clear field on focus unless new text has been entered.
+    if (facebook_status_count == 1) {
+      facebook_status_value = "";
+      $('div#facebook_status_update').find('input#edit-status').val(facebook_status_value);
+    }
+    $('div#facebook_status_update').find('input#edit-status').keypress(function() {
+      facebook_status_count = 0;
+    });
+    //Set to original value if field is empty and the textfield is closed.
+    $('div#facebook_status_update').find('a.facebook_status_slide').click(function() {
+      if ($('div#facebook_status_update').find('input#edit-status').val() == "") {
+        facebook_status_value = facebook_status_original_value;
+        $('div#facebook_status_update').find('input#edit-status').val(facebook_status_value);
+      }
+    });
+  }
 }
