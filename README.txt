8913d2863a9de011427c7bd8ec7dd8c0d5097eca
Isaac Sukin <ice@icecreamyou.com>
2009-05-25 05:59:44 +0000

    MANY API CHANGES!
    *Removed slider
    _render hook now applies to the status on the update form
    JS now loads on all update forms
    Better formatting for time
    Removed $linked parameter from theme('facebook_status_item')
    Removed redundant and deprecated code comments
    *Added option to use a textarea instead of a textfield
    *Added JavaScript-driven character counting
    Improved JavaScript handling of status update form
    Changed redundant title of a setting
    Status length can now be up to 1000 characters
    "status/openapi" changed to "statuses/openapi"
    Added "edit" pages
    *#425628 Allow input filters on statuses
    Added flood control and permissions for using the Open API
    *Added ability to post on others' profiles.
    Added a delete page (uses edit permissions)
    Corrected missing arguments in return value from hook_theme()
    Delete users' posts on user deletion
    Lots of other minor improvements and bug fixes

--- README.txt
+++ README.txt
@@ -1,12 +1,14 @@
 // $Id$
 
 The Facebook-style Statuses module provides users with a short textual "status"
-that they can use to express themselves. Comprehensive settings, APIs, theming,
-and module integration are included.
+that they can use to express themselves. Users can also post on other users'
+profiles. Comprehensive settings, APIs, theming, and module integration are
+included.
 
 Extensive admin documentation is available at http://drupal.org/node/421128
 Thorough API/developer documentation is at http://drupal.org/node/421336
 Visit the project page at http://drupal.org/project/facebook_status
 Check out the issue queue at http://drupal.org/project/issues/facebook_status
 
-IceCreamYou wrote and maintains the module.
+IceCreamYou wrote and maintains the module, and is available for paid
+customizations. Contact him at http://drupal.org/user/201425/contact
--- facebook_status.js
+++ facebook_status.js
@@ -2,30 +2,45 @@
 
 Drupal.behaviors.facebookStatus = function (context) {
   facebook_status_count = 1;
-  facebook_status_original_value = $('div#facebook_status_update').find('input#edit-status').val();
-  //Show or hide status form.
-  $('div#facebook_status_update').find('.facebook_status_hide').hide().end().find('a.facebook_status_slide').click(function() {
-    $('.facebook_status_hide').slideToggle();
+  facebook_status_original_value = $('div#facebook_status_update').find('#edit-status').val();
+  //Clear the status field the first time it's in focus if it hasn't been changed.
+  $('div#facebook_status_update').find('#edit-status').focus(function() {
+    facebook_status_value = $('div#facebook_status_update').find('#edit-status').val();
+    if (facebook_status_count == 1 && facebook_status_value == facebook_status_original_value) {
+      $('div#facebook_status_update').find('#edit-status').val("");
+      facebook_status_count = 0;
+    }
   });
-  $('div#facebook_status_update').find('input#edit-status').focus(function() {
-    facebook_status_form_clear();
+  //Put the original status text back in the field if it is blank and the slider is clicked.
+  $('div#facebook_status_update').find('#facebook_status_slider').click(function() {
+    facebook_status_value = $('div#facebook_status_update').find('#edit-status').val();
+    if (facebook_status_count != 2 && facebook_status_value == '') {
+      facebook_status_count = 2;
+      $('div#facebook_status_update').find('#edit-status').val(facebook_status_original_value);
+    }
   });
-}
-function facebook_status_form_clear() {
-  facebook_status_value = $('div#facebook_status_update').find('input#edit-status').val();
-  //Clear field on focus unless new text has been entered.
-  if (facebook_status_count == 1) {
-    facebook_status_value = "";
-    $('div#facebook_status_update').find('input#edit-status').val(facebook_status_value);
-  }
-  $('div#facebook_status_update').find('input#edit-status').keypress(function() {
-    facebook_status_count = 0;
-  });
-  //Set to original value if field is empty and the textfield is closed.
-  $('div#facebook_status_update').find('a.facebook_status_slide').click(function() {
-    if ($('div#facebook_status_update').find('input#edit-status').val() == "") {
-      facebook_status_value = facebook_status_original_value;
-      $('div#facebook_status_update').find('input#edit-status').val(facebook_status_value);
+  //Show an indicator of the remaining characters.
+  $('div#facebook_status_update').find('#edit-status').keypress(function(fbss_key) {
+    //The following logic corrects counting errors.
+    fbss_remaining = Drupal.settings.facebook_status.maxlength - $('div#facebook_status_update').find('#edit-status').val().length - 1;
+    if (fbss_key.which == 8 && fbss_remaining + 1 < Drupal.settings.facebook_status.maxlength) {
+      fbss_remaining += 2;
+    }
+    else if (fbss_key.which == 8 && isNaN($('div#facebook_status_update').find('#edit-status').val().charCodeAt())) {
+      fbss_remaining++;
+    }
+    else if (fbss_key.which != 8 && Drupal.settings.facebook_status.ttype == 'textfield' && fbss_remaining < 0) {
+      fbss_remaining = 0;
+    }
+    //Outputs translated remaining characters.
+    var fbss_translations = new Array();
+    fbss_translations['%chars'] = fbss_remaining;
+    if (fbss_remaining >= 0) {
+      $('div#facebook_status_update').find('#facebook_status_chars').html(Drupal.t('(%chars)', fbss_translations));
+    }
+    else {
+      fbss_newval = '<span class="facebook_status_negative">'+ Drupal.t('(%chars)', fbss_translations) +'</span>';
+      $('div#facebook_status_update').find('#facebook_status_chars').html(fbss_newval);
     }
   });
 }
