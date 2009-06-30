089dba023976c9fe1f65e2e9492a015478d0c5c4
Isaac Sukin <ice@icecreamyou.com>
2009-06-30 20:04:58 +0000

    Removed facebook_status_flag_create() from hook_theme() Improved character counter efficiency and accuracy Clear status update form after an AHAH-update Allow users to delete posts on their own profiles with the "edit own status" permission Updated default facebook_status and facebook_status_recent views to include edit/delete links Default facebook_status view now takes either UIDs or Usernames as arguments NOTE: revert your views and then run update.php or clear your cache after upgrading

--- facebook_status.js
+++ facebook_status.js
@@ -1,46 +1,43 @@
 // $Id$
-
 Drupal.behaviors.facebookStatus = function (context) {
-  facebook_status_count = 1;
-  facebook_status_original_value = $('div#facebook_status_update').find('#edit-status').val();
+  $facebook_status_field = $('#facebook_status_update').find('#edit-status');
+  $facebook_status_chars = $('#facebook_status_update').find('#facebook_status_chars');
+  facebook_status_original_value = $facebook_status_field.val();
+  fbss_maxlen = Drupal.settings.facebook_status.maxlength;
   //Clear the status field the first time it's in focus if it hasn't been changed.
-  $('div#facebook_status_update').find('#edit-status').focus(function() {
-    facebook_status_value = $('div#facebook_status_update').find('#edit-status').val();
-    if (facebook_status_count == 1 && facebook_status_value == facebook_status_original_value) {
-      $('div#facebook_status_update').find('#edit-status').val("");
-      facebook_status_count = 0;
+  $facebook_status_field.one('focus', function() {
+    facebook_status_value = $facebook_status_field.val();
+    if (facebook_status_value == facebook_status_original_value) {
+      $facebook_status_field.val('');
+      fbss_print_remaining(fbss_maxlen);
     }
   });
-  //Put the original status text back in the field if it is blank and the slider is clicked.
-  $('div#facebook_status_update').find('#facebook_status_slider').click(function() {
-    facebook_status_value = $('div#facebook_status_update').find('#edit-status').val();
-    if (facebook_status_count != 2 && facebook_status_value == '') {
-      facebook_status_count = 2;
-      $('div#facebook_status_update').find('#edit-status').val(facebook_status_original_value);
+  //Restore original status text if the field is blank and the slider is clicked.
+  $('#facebook_status_slider').click(function() {
+    facebook_status_value = $facebook_status_field.val();
+    if (facebook_status_value == '') {
+      $facebook_status_field.val(facebook_status_original_value);
+      fbss_print_remaining(fbss_maxlen - facebook_status_original_value.length);
     }
   });
-  //Show an indicator of the remaining characters.
-  $('div#facebook_status_update').find('#edit-status').keypress(function(fbss_key) {
-    //The following logic corrects counting errors.
-    fbss_remaining = Drupal.settings.facebook_status.maxlength - $('div#facebook_status_update').find('#edit-status').val().length - 1;
-    if (fbss_key.which == 8 && fbss_remaining + 1 < Drupal.settings.facebook_status.maxlength) {
-      fbss_remaining += 2;
-    }
-    else if (fbss_key.which == 8 && isNaN($('div#facebook_status_update').find('#edit-status').val().charCodeAt())) {
-      fbss_remaining++;
-    }
-    else if (fbss_key.which != 8 && Drupal.settings.facebook_status.ttype == 'textfield' && fbss_remaining < 0) {
+  //Count remaining characters.
+  $facebook_status_field.keyup(function(fbss_key) {
+    fbss_remaining = fbss_maxlen - $facebook_status_field.val().length;
+    if (Drupal.settings.facebook_status.ttype == 'textfield' && fbss_remaining < 0) {
       fbss_remaining = 0;
     }
-    //Outputs translated remaining characters.
-    var fbss_translations = new Array();
-    fbss_translations['%chars'] = fbss_remaining;
-    if (fbss_remaining >= 0) {
-      $('div#facebook_status_update').find('#facebook_status_chars').html(Drupal.t('(%chars)', fbss_translations));
-    }
-    else {
-      fbss_newval = '<span class="facebook_status_negative">'+ Drupal.t('(%chars)', fbss_translations) +'</span>';
-      $('div#facebook_status_update').find('#facebook_status_chars').html(fbss_newval);
-    }
+    fbss_print_remaining(fbss_remaining);
   });
+}
+//Change remaining character count.
+function fbss_print_remaining(fbss_remaining) {
+  var fbss_translations = new Array();
+  fbss_translations['%chars'] = fbss_remaining;
+  if (fbss_remaining >= 0) {
+    $facebook_status_chars.html(Drupal.t('%chars characters remaining', fbss_translations));
+  }
+  else {
+    fbss_newval = '<span class="facebook_status_negative">'+ Drupal.t('%chars characters remaining', fbss_translations) +'</span>';
+    $facebook_status_chars.html(fbss_newval);
+  }
 }
