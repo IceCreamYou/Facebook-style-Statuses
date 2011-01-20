cf0bb9a363b444f502dcf423b74ccb75ac461a33
Isaac Sukin <ice@icecreamyou.com>
2011-01-20 02:53:40 +0000

    Updated JS and CSS to match 3.x markup Fixed facebook_status_form_display() could mess up the context

--- resources/facebook_status.js
+++ resources/facebook_status.js
@@ -2,21 +2,19 @@
 var allowClickRefresh = true;
 Drupal.behaviors.facebookStatus = function (context) {
   var initialLoad = false;
-  //Drupal passes document as context on page load.
+  // Drupal passes document as context on page load.
   if (context == document) {
     initialLoad = true;
   }
-  //Make sure we can run context.find().
-  if (!(context instanceof jQuery)) {
-    context = $(context);
-  }
+  // Make sure we can run context.find().
+  var ctxt = $(context);
   facebook_status_submit_disabled = false;
-  var $facebook_status_field = context.find('.facebook_status_text:first');
+  var $facebook_status_field = ctxt.find('.facebook-status-text:first');
   var facebook_status_original_value = $facebook_status_field.val();
   var fbss_maxlen = Drupal.settings.facebook_status.maxlength;
   var refreshIDs = Drupal.settings.facebook_status.refreshIDs;
   if ($.fn.autogrow && $facebook_status_field) {
-    //jQuery Autogrow plugin integration.
+    // jQuery Autogrow plugin integration.
     $facebook_status_field.autogrow({expandTolerance: 2});
   }
   if (Drupal.settings.facebook_status.autofocus) {
@@ -28,25 +26,25 @@ Drupal.behaviors.facebookStatus = function (context) {
     }
   }
   else {
-    $.each(context.find('.facebook_status_text_main'), function(i, val) {
-      $(this).addClass('facebook_status_faded');
+    $.each(ctxt.find('.facebook-status-text-main'), function(i, val) {
+      $(this).addClass('facebook-status-faded');
     });
-    //Clear the status field the first time it's in focus if it hasn't been changed.
-    context.find('.facebook_status_text_main').one('focus', function() {
+    // Clear the status field the first time it's in focus if it hasn't been changed.
+    ctxt.find('.facebook-status-text-main').one('focus', function() {
       if ($(this).val() == facebook_status_original_value) {
         $(this).val('');
         fbss_print_remaining(fbss_maxlen, $(this).parent().next());
       }
-      $(this).removeClass('facebook_status_faded');
+      $(this).removeClass('facebook-status-faded');
     });
   }
-  //Fix bad redirect destinations.
-  context.find('.facebook_status_edit_delete a').each(function() {
-    //window.location.href doesn't work for sites not in the webroot with weird server configurations.
+  // Fix bad redirect destinations.
+  ctxt.find('.facebook-status-edit a, .facebook-status-delete a').each(function() {
+    // window.location.href doesn't work for sites not in the webroot with weird server configurations.
     var redirectTo = window.location.pathname.substring(Drupal.settings.basePath.length) + window.location.search;
     $(this).attr('href', $(this).attr('href').split('?')[0] +'?destination='+ escape(redirectTo));
   });
-  context.find('a.facebook_status_conversation_link').each(function() {
+  ctxt.find('a.facebook-status-respond, a.facebook-status-repost').each(function() {
     var loc = $(this).attr('href').split('?'), base = loc[0], query = '';
     if (loc[1]) {
       var q = loc[1].split('&');
@@ -69,15 +67,15 @@ Drupal.behaviors.facebookStatus = function (context) {
       $(this).attr('href', base + query);
     }
   });
-  //React when a status is submitted.
-  context.find('#facebook_status_replace').bind('ahah_success', function(context) {
+  // React when a status is submitted.
+  ctxt.find('#facebook-status-replace').bind('ahah_success', function(context) {
     if ($(context.target).html() != $(this).html()) {
       return;
     }
     if (Drupal.heartbeat) {
       Drupal.heartbeat.pollMessages();
     }
-    //Refresh elements by re-loading the current page and replacing the old version with the updated version.
+    // Refresh elements by re-loading the current page and replacing the old version with the updated version.
     var loaded = {};
     if (refreshIDs && refreshIDs != undefined) {
       var loaded2 = {};
@@ -90,14 +88,14 @@ Drupal.behaviors.facebookStatus = function (context) {
           }
         }
       });
-      //IE will cache the result unless we add an identifier (in this case, the time).
+      // IE will cache the result unless we add an identifier (in this case, the time).
       $.get(window.location.pathname +"?ts="+ (new Date()).getTime(), function(data, textStatus) {
-        //From load() in jQuery source. We already have the scripts we need.
+        // From load() in jQuery source. We already have the scripts we need.
         var new_data = data.replace(/<script(.|\s)*?\/script>/g, "");
-        //From ahah.js. Apparently Safari crashes with just $().
+        // From ahah.js. Apparently Safari crashes with just $().
         var new_content = $('<div></div>').html(new_data);
         if (textStatus != 'error' && new_content) {
-          //Replace relevant content in the viewport with the updated version.
+          // Replace relevant content in the viewport with the updated version.
           $.each(refreshIDs, function(i, val) {
             if (val && val != undefined) {
               if ($.trim(val) != '' && loaded[val] !== true) {
@@ -119,7 +117,7 @@ Drupal.behaviors.facebookStatus = function (context) {
       $('.fbss-remove-me').remove();
     }
   });
-  //On document load, add a refresh link where applicable.
+  // On document load, add a refresh link where applicable.
   if (initialLoad && refreshIDs && Drupal.settings.facebook_status.refreshLink) {
     var loaded = {};
     $.each(refreshIDs, function(i, val) {
@@ -129,53 +127,53 @@ Drupal.behaviors.facebookStatus = function (context) {
           var element = $(val);
           element.wrap('<div></div>');
           var rlink = '<a href="'+ window.location.href +'">'+ Drupal.t('Refresh') +'</a>';
-          element.parent().after('<div class="facebook_status_refresh_link">'+ rlink +'</div>');
+          element.parent().after('<div class="facebook-status-refresh-link">'+ rlink +'</div>');
         }
       }
     });
   }
-  //Refresh views appropriately.
-  context.find('.facebook_status_refresh_link a').click(function() {
+  // Refresh views appropriately.
+  ctxt.find('.facebook-status-refresh-link a').click(function() {
     if (allowClickRefresh) {
       allowClickRefresh = false;
       setTimeout('allowRefresh()', 2000);
       $(this).after('<div class="fbss-remove-me ahah-progress ahah-progress-throbber"><div class="throbber">&nbsp;</div></div>');
-      $('#facebook_status_replace').trigger('ahah_success', {target: '#facebook_status_replace'});
+      $('#facebook-status-replace').trigger('ahah_success', {target: '#facebook-status-replace'});
     }
     return false;
   });
-  //Restore original status text if the field is blank and the slider is clicked.
-  context.find('.facebook_status_slider').click(function() {
-    if ($(this).next().find('.facebook_status_text').val() == '') {
-      $(this).next().find('.facebook_status_text').val(facebook_status_original_value);
-      fbss_print_remaining(fbss_maxlen - facebook_status_original_value.length, $(this).next().next());
+  // Restore original status text if the field is blank and the slider is clicked.
+  ctxt.find('.facebook-status-intro').click(function() {
+    if ($(this).next().find('.facebook-status-text').val() == '') {
+      $(this).next().find('.facebook-status-text').val(facebook_status_original_value);
+      fbss_print_remaining(fbss_maxlen - facebook_status_original_value.length, $(this).parents('.facebook-status-update').find('.facebook-status-chars'));
     }
   });
-  //Count remaining characters.
-  context.find('.facebook_status_text').keypress(function(fbss_key) {
+  // Count remaining characters.
+  ctxt.find('.facebook-status-text').keypress(function(fbss_key) {
     var th = $(this);
     setTimeout(function() {
       var fbss_remaining = fbss_maxlen - th.val().length;
       if (Drupal.settings.facebook_status.ttype == 'textfield' && fbss_remaining < 0) {
         fbss_remaining = 0;
       }
-      fbss_print_remaining(fbss_remaining, th.parent().next());
+      fbss_print_remaining(fbss_remaining, $(this).parents('.facebook-status-update').find('.facebook-status-chars'));
     }, 10);
   });
 }
-//Change remaining character count.
+// Change remaining character count.
 function fbss_print_remaining(fbss_remaining, where) {
   if (fbss_remaining >= 0) {
     where.html(Drupal.formatPlural(fbss_remaining, '1 character left', '@count characters left', {}));
     if (facebook_status_submit_disabled) {
-      $('.facebook_status_submit').attr('disabled', false);
+      $('.facebook-status-submit').attr('disabled', false);
       facebook_status_submit_disabled = false;
     }
   }
   else {
-    where.html('<span class="facebook_status_negative">'+ Drupal.formatPlural(Math.abs(fbss_remaining), '-1 character left', '-@count characters left', {}) +'</span>');
+    where.html('<span class="facebook-status-negative">'+ Drupal.formatPlural(Math.abs(fbss_remaining), '-1 character left', '-@count characters left', {}) +'</span>');
     if (!facebook_status_submit_disabled) {
-      $('.facebook_status_submit').attr('disabled', true);
+      $('.facebook-status-submit').attr('disabled', true);
       facebook_status_submit_disabled = true;
     }
   }
