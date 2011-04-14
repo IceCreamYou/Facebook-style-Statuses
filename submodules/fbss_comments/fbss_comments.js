db50562aed7a7ba5a11236f4fdd1d7e89cd7db6b
Isaac Sukin <ice@icecreamyou.com>
2011-04-14 02:52:16 -0400

    Issue #1055666 - "field is required" error handling and also a blank error message by IceCreamYou
    (Also adds some nice JS handling for focusing on textareas and prohibiting submitting an empty comment.)

--- submodules/fbss_comments/fbss_comments.js
+++ submodules/fbss_comments/fbss_comments.js
@@ -1,17 +1,31 @@
 Drupal.behaviors.fbss_comments = function (context) {
-  $('.fbss-comments-show-comment-form').one('click', function() {
+  $(context).find('.fbss-comments-show-comment-form').one('click', function() {
     $(this).hide();
     $('#'+ this.id +' + div').show();
+    $(this).next().find('.fbss-comments-textarea').focus();
     return false;
   });
-  $('.fbss-comments-show-comment-form-inner').one('click', function() {
+  $(context).find('.fbss-comments-show-comment-form-inner').one('click', function() {
     $(this).hide();
     $('#'+ this.id +' + div').show();
+    $(this).parents('form').find('.fbss-comments-textarea').focus();
     return false;
   });
-  $('a.fbss-comments-show-comments').one('click', function() {
+  $(context).find('a.fbss-comments-show-comments').one('click', function() {
     $(this).hide();
     $('#'+ this.id +' ~ div.fbss-comments-hide').show();
     return false;
+  });
+  $(context).find('.fbss-comments-submit').attr('disabled', true);
+  $(context).find('.fbss-comments-textarea').keypress(function(key) {
+    var th = $(this);
+    setTimeout(function() {
+      if (th.val().length > 0) {
+        th.parents('form').find('input').attr('disabled', false);
+      }
+      else {
+        th.parents('form').find('input').attr('disabled', true);
+      }
+    }, 10);
   });
 }
